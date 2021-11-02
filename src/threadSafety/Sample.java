bbf5a8e2e4f6105308ec6e62ea16fc1fc434978e
201402407 <banner4@naver.com>
2021-11-02 15:38:46 +0900

    자바 동기화 작성

--- src/threadSafety/Sample.java
+++ src/threadSafety/Sample.java
@@ -1,12 +1,10 @@
 package threadSafety;
 
 public class Sample {
-	
 	public static void main(String args[]) throws InterruptedException {
 		question1();
 		question2();
 		question3();
-		Thread.currentThread().sleep(2500);	// 임시로 question3 함수 쓰레딩 종료 시 호출을 위해 선언
 	}
 	
 	static void question1() {
@@ -34,7 +32,7 @@ public class Sample {
 	
 	static void question3() {
 		System.out.println("[QUESTION 3] Not Thread-safe 한 경우(동기화X)");
-		System.out.println("[ANSWER] not thread-safe 하기 때문에 잔액이 음수여도 출금 함수가 호출된다.");
+		System.out.println("[ANSWER] not thread-safety 하기 때문에 매 번 잔액 결과가 다르다.");
 		Banking banking = new Banking(new BankingCallback());
 		Thread thread1 = new Thread(banking);
 		Thread thread2 = new Thread(banking);
@@ -47,19 +45,29 @@ public class Sample {
 	
 	static void question4() {
 		System.out.println("[QUESTION 4] Synchronized 방법 중 synchronized 함수를 만들어 사용");
-//		Banking banking = new Banking(new BankingCallback());
-//		Thread thread1 = new Thread(banking);
-//		Thread thread2 = new Thread(banking);
-//		thread1.setName("Thread 1번");
-//		thread2.setName("Thread 2번");
-//		thread1.start();
-//		thread2.start();
+		System.out.println("[ANSWER] thread-safety 하기 때문에 잔액 결과가 같다.(Thread1, 2 실행 시간에 따라 누가 어떤 잔액을 가지는지는 달라지지만, 결과는 동일하다)");
+		System.out.println("[ANSWER] Synchronized는 객체 내부 전체에 lock을 걸기 때문에, 다른 쓰레드는 객체 자체를 사용하려면 기다려야 한다.");
+		BankingSync banking = new BankingSync(new BankingCallback());
+		Thread thread1 = new Thread(banking);
+		Thread thread2 = new Thread(banking);
+		thread1.setName("Thread 1번");
+		thread2.setName("Thread 2번");
+		thread1.start();
+		thread2.start();
 //		System.out.println("------------------------------------------------------------------------------");
 	}
 	
 	static void question5() {
 		System.out.println("[QUESTION 5] Synchronized 방법 중 synchronized lock을 만들어 사용");
-		System.out.println("[ANSWER] not thread-safe 하기 때문에 종료 구문이 2개 나올 수 있다.");
+		System.out.println("[ANSWER] thread-safety 하기 때문에 잔액 결과가 같다.(Thread1, 2 실행 시간에 따라 누가 어떤 잔액을 가지는지는 달라지지만, 결과는 동일하다)");
+		System.out.println("[ANSWER] 함수 내부에 별도 객체를 만들어 lock을 걸기 때문에, 다른 쓰레드는 해당 함수 내부에서 객체 종료를 기다려야 한다.");
+		BankingSync2 banking = new BankingSync2(new BankingCallback());
+		Thread thread1 = new Thread(banking);
+		Thread thread2 = new Thread(banking);
+		thread1.setName("Thread 1번");
+		thread2.setName("Thread 2번");
+		thread1.start();
+		thread2.start();
 		System.out.println("------------------------------------------------------------------------------");
 	}
 	
@@ -68,24 +76,34 @@ public class Sample {
 		System.out.println("[ANSWER] not thread-safe 하기 때문에 종료 구문이 2개 나올 수 있다.");
 		System.out.println("------------------------------------------------------------------------------");
 	}
+	
+    static void tempString() {
+    	System.out.println(Thread.currentThread().getName() + " TEMP STRING 호출!");
+    }
 }
 
 // 쓰레드 콜백함수 호출 커스텀
 class BankingCallback {
 	int finishThreadCount = 0;
 	
-	void callback(Thread thread) {
+	void callback(Thread thread, int questionNumber) {
 		System.out.println(thread.getName() + " 쓰레드 종료!");
 		finishThreadCount++;
 		if(finishThreadCount == 2) {
 			System.out.println("------------------------------------------------------------------------------");
+			if(questionNumber == 3) {
+				Sample.question4();
+			}
+			if(questionNumber == 4) {
+				Sample.question5();
+			}
 		}
 	}
 }
 
 class Banking implements Runnable {
 	BankingCallback cb;
-    int money = 600; 
+    int money = 700; 
       
     Banking(BankingCallback cb) {
     	this.cb = cb;
@@ -95,23 +113,101 @@ class Banking implements Runnable {
     public void run() {
         while(money > 0){
             // 100, 200, 300 중의 한 값을 임의로 선택해서 출금(withDraw)한다.
-            int money = (int)(Math.random() * 3 + 1) * 100;
+//            int money = (int)(Math.random() * 3 + 1) * 100;
+        	int money = 300;
             withdraw(money);
         }
         
-        this.cb.callback(Thread.currentThread());
+        this.cb.callback(Thread.currentThread(), 3);
     }
     
     private void withdraw(int money) {
+    	Sample.tempString();
+    	
     	try {
             Thread thread = Thread.currentThread();
             System.out.println(thread.getName() + " 출금 금액 => " + money);
-            Thread.sleep(500);
+            Thread.sleep(200);
             this.money -= money;
             System.out.println(thread.getName()+" 잔액 => " + this.money);
         }
     	catch (Exception e) {
         	e.printStackTrace();
         }
+    }
+}
+
+class BankingSync implements Runnable {
+	BankingCallback cb;
+    int money = 700; 
+      
+    BankingSync(BankingCallback cb) {
+    	this.cb = cb;
+    }
+    
+    @Override
+    public void run() {
+        while(money > 0){
+            // 100, 200, 300 중의 한 값을 임의로 선택해서 출금(withDraw)한다.
+//            int money = (int)(Math.random() * 3 + 1) * 100;
+        	int money = 300;
+            withdraw(money);
+        }
+        
+        this.cb.callback(Thread.currentThread(), 4);
+    }
+    
+    private synchronized void withdraw(int money) {
+    	Sample.tempString();
+    	
+    	try {
+            Thread thread = Thread.currentThread();
+            System.out.println(thread.getName() + " 출금 금액 => " + money);
+            Thread.sleep(200);
+            this.money -= money;
+            System.out.println(thread.getName()+" 잔액 => " + this.money);
+        }
+    	catch (Exception e) {
+        	e.printStackTrace();
+        }
+    }
+}
+
+class BankingSync2 implements Runnable {
+	BankingCallback cb;
+    int money = 700; 
+      
+    BankingSync2(BankingCallback cb) {
+    	this.cb = cb;
+    }
+    
+    @Override
+    public void run() {
+        while(money > 0){
+            // 100, 200, 300 중의 한 값을 임의로 선택해서 출금(withDraw)한다.
+//            int money = (int)(Math.random() * 3 + 1) * 100;
+        	int money = 300;
+            withdraw(money);
+        }
+        
+        this.cb.callback(Thread.currentThread(), 5);
+    }
+    
+    private void withdraw(int money) {
+    	Sample.tempString();
+    	
+    	synchronized(this) {	// 별도 객체 생성해서 해당 객체에만 Synchronized 설정
+    		try {
+                Thread thread = Thread.currentThread();
+                System.out.println(thread.getName() + " 출금 금액 => " + money);
+                Thread.sleep(200);
+                this.money -= money;
+                System.out.println(thread.getName()+" 잔액 => " + this.money);
+            }
+        	catch (Exception e) {
+            	e.printStackTrace();
+            }
+    	}
+    	
     }
 }
